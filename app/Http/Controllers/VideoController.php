2038d107c029001eeeda529e8b4640d42a4d5fc4
Ashkan Eghdam <ashkan.eghdam73@gmail.com>
2021-12-11 11:16:44 +0330

    initial-VideoController-class

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Http\Controllers;
+
+class VideoController extends Controller{
+    public function index(){
+        dd("Hello from VideoController class");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/panel', function () {
+    dd("Hello from panel");
+});
+
+Route::get('/videos', [VideoController::class, 'index']);
