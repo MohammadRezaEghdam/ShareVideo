d7045cc94c07349baa3519822ca09c8da853c4c9
Ashkan Eghdam <ashkan.eghdam73@gmail.com>
2021-12-12 13:05:49 +0330

    implement-simple-video-table&show-data

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -1,13 +1,14 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Video;
 use Illuminate\View\View;
 
 class VideoController extends Controller{
+
     public function index(){
-        $videos = ['A', 'B', 'C', 'D', 'E', 'F'];
-        return View('videos',[
-            'data' => $videos
-        ]);
+        $videos = Video::all();
+        dd($videos);
     }
+
 }
