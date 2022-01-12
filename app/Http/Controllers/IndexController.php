cd7ecb657fb1d4500556fec9003d32f53abd01da
Ashkan Eghdam <ashkan.eghdam73@gmail.com>
2022-01-12 11:45:22 +0330

    implement-components

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,11 +14,10 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $videos = Video::all()->last()->take(6)->get();
         $mostPopularVideos = Video::all()->random(6);
         $mostViewedVideos = Video::all()->random(6);
 
-        return view('index', compact('videos', 'mostPopularVideos', 'mostViewedVideos'));
+        return view('index', compact('mostPopularVideos', 'mostViewedVideos'));
     }
 
     /**
