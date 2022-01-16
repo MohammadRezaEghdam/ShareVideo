47e9e2317af0a4bbcbfd5bf301eeb5cb08d01be0
Ashkan Eghdam <ashkan.eghdam73@gmail.com>
2022-01-16 11:01:45 +0330

    change-errors-lang-to-fa

---
+++ resources/lang/fa/validation.php
@@ -0,0 +1,14 @@
+<?php
+
+return[
+    'required' => ' :attribute الزامی می باشد.',
+    'attributes' => [
+        'name' => 'نام ویدئو',
+        'length' => 'مدت زمان ویدئو',
+        'slug' => 'نام یکتا',
+        'url' => 'آدرس ویدئو',
+        'description' => 'توضیحات',
+        'thumbnail' => 'تصویر زمینه'
+    ]
+
+];
