3bd1c770959557a1aef6fc0fc3f2230414bf4f79
Ashkan Eghdam <ashkan.eghdam73@gmail.com>
2022-03-21 17:31:13 +0330

    make register page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,11 +33,14 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
+        
+        dd($request->all());
 
         $user = User::create([
             'name' => $request->name,
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,50 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('auth-layout')
+@section('class-body', 'sing-up-page')
+@section('content')
+    <div id="log-in" class="site-form log-in-form">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <div id="log-in-head">
+            <h1>ثبت نام</h1>
+            <div id="logo"><a href="{{ route('index') }}"><img src="img/logo.png" alt=""></a></div>
+        </div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+        <div class="form-output">
+            <x-validation-errors></x-validation-errors>
+            <form action="{{ route('register.store') }}" method="POST">
+                @csrf
+                <div class="form-group label-floating">
+                    <label class="control-label">نام</label>
+                    <input class="form-control" name="name" placeholder="" type="text">
+                </div>
+                <div class="form-group label-floating">
+                    <label class="control-label">ایمیل</label>
+                    <input name="email" class="form-control" placeholder="" type="email">
+                </div>
+                <div class="form-group label-floating">
+                    <label class="control-label">رمز عبور</label>
+                    <input name="password" class="form-control" placeholder="" type="password">
+                </div>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+                <div class="form-group label-floating">
+                    <label class="control-label">تأیید رمز عبور</label>
+                    <input name="password_confirmation" class="form-control" placeholder="" type="password">
+                </div>
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+                <div class="remember">
+                    <div class="checkbox">
+                        <label>
+                            <input name="remember" type="checkbox">
+                            مرا به خاطر بسپار
+                        </label>
+                    </div>
+                </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <button type="submit" class="btn btn-lg btn-primary full-width">ثبت نام</button>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                <div class="or"></div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+                <p>شما یک حساب کاربری دارید؟ <a href="{{ route('login.create') }}"> ورود!</a> </p>
+            </form>
+        </div>
+    </div>
+@endsection
